d2b1bbb945e1d2b2bf794a831737f62a7867c54d
Jaime <jaicarsan1@alum.us.es>
2024-07-26 13:08:35 +0200

    Añadidas excepciones a ejercicio 3

--- ejercicio3.py
+++ ejercicio3.py
@@ -5,16 +5,18 @@ def leer_tarifa(tarifa: float) -> float:
     return tarifa
 
 def calcular_sueldo(horas: float, tarifa: float) -> float:
+    if tarifa <= 0:
+        raise Exception("La tarifa debe ser superior a 0")
     if horas > 40.:
         base = 40. * tarifa
         extra = (horas - 40.) * tarifa * 1.5
         return base + extra
-    elif 0. < horas <= 40.:
+    elif 0. <= horas <= 40.:
         return horas * tarifa
     else:
-        print("Las horas trabajadas no pueden ser negativas")
+        raise Exception("Las horas trabajadas no pueden ser negativas")
 
-horas = leer_horas_trabajadas(41)
+horas = leer_horas_trabajadas(-10)
 tarifa = leer_tarifa(20)
 sueldo = calcular_sueldo(horas, tarifa)
 print(sueldo)
